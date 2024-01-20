d9672fbdd275813aec6caae19fdc965dfef5e411
Ayan <imluckyayan@gmail.com>
2024-01-20 13:32:03 +0530

    Test API key cleaned

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,1 +1,2 @@
-const openWeatherAPIKey = '9815c7ee62a91465bcf92bd4d62c23a2';
+// TODO: Input the OpenWeather API Key here from https://openweathermap.org
+const openWeatherAPIKey = 'OPENWEATHER_API_KEY';
